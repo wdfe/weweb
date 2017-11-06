e5a49159fb8258ba0bfab09978ea947f5bbf5595
Junjie Ding <djjoriens@gmail.com>
2017-11-06 10:22:50 +0800

    refactor: use more functions in bin/weweb

--- webpack.config.js
+++ webpack.config.js
@@ -62,7 +62,7 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
-          presets: ['es2015','env','stage-0']
+          presets: ['env', 'stage-0']
         }
       },
       {
